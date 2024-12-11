d497361d1997a30658fe42d995492d7eb44e9b53
cesar augusto gadelha <cesar_ags@outlook.com>
2024-12-11 18:36:44 -0300

    Comment out MongoDbContextTest class for future use
    The entire MongoDbContextTest class in MongoDbContextTest.cs has been commented out. This includes the private field _mongoDbContext, the constructor, and the test method Test_AddProduto_Integration. The comments indicate that the code is temporarily disabled but preserved for potential future use.

--- src/Api.Catalogo.Tests/MongoDbContextTest.cs
+++ src/Api.Catalogo.Tests/MongoDbContextTest.cs
@@ -10,29 +10,29 @@ namespace Api.Catalogo.Tests;
 public class MongoDbContextTest
 {
 
-    private readonly IMongoDbContext _mongoDbContext;
+    //private readonly IMongoDbContext _mongoDbContext;
 
-    public MongoDbContextTest()
-    {
-        var settings = Options.Create(new MongoDbSettings
-        {
-            ConnectionString = "mongodb://localhost:27017",
-            DatabaseName = "MediatrDemo"
-        });
+    //public MongoDbContextTest()
+    //{
+    //    var settings = Options.Create(new MongoDbSettings
+    //    {
+    //        ConnectionString = "mongodb://localhost:27017",
+    //        DatabaseName = "MediatrDemo"
+    //    });
 
-        _mongoDbContext = new MongoDbContext(settings);
-    }
+    //    _mongoDbContext = new MongoDbContext(settings);
+    //}
 
 
-    [Fact(DisplayName = "Teste 01 - Deve se conectar ao banco com sucesso.")]
-    [Trait("Catalogo", "MongoDbContextTest")]
-    public async Task Test_AddProduto_Integration()
-    {
-        // Act: Pega a lista de coleções do banco de dados para verificar a conexão
-        var collectionNames =  _mongoDbContext?.Database?.ListCollectionNames().ToList();
+    //[Fact(DisplayName = "Teste 01 - Deve se conectar ao banco com sucesso.")]
+    //[Trait("Catalogo", "MongoDbContextTest")]
+    //public async Task Test_AddProduto_Integration()
+    //{
+    //    // Act: Pega a lista de coleções do banco de dados para verificar a conexão
+    //    var collectionNames =  _mongoDbContext?.Database?.ListCollectionNames().ToList();
 
-        // Assert: Verifica se a conexão com o banco foi bem-sucedida ao tentar acessar coleções
-        Assert.NotNull(collectionNames);
+    //    // Assert: Verifica se a conexão com o banco foi bem-sucedida ao tentar acessar coleções
+    //    Assert.NotNull(collectionNames);
         
-    }
+    //}
 }
